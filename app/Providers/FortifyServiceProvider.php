2b0841e13da193442574e525284d94b2ad5a994d
yushi <xyu@yushinoMac-mini.local>
2026-02-13 16:21:37 +0900

    wip user認証を実装中

--- app/Providers/FortifyServiceProvider.php
+++ app/Providers/FortifyServiceProvider.php
@@ -12,7 +12,11 @@ use Illuminate\Support\Facades\RateLimiter;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Str;
 use Laravel\Fortify\Actions\RedirectIfTwoFactorAuthenticatable;
+use Laravel\Fortify\Contracts\LoginResponse;
+use Laravel\Fortify\Contracts\LogoutResponse;
+use Laravel\Fortify\Contracts\RegisterResponse;
 use Laravel\Fortify\Fortify;
+use Illuminate\Http\JsonResponse;
 
 class FortifyServiceProvider extends ServiceProvider
 {
@@ -36,13 +40,36 @@ class FortifyServiceProvider extends ServiceProvider
         Fortify::redirectUserForTwoFactorAuthenticationUsing(RedirectIfTwoFactorAuthenticatable::class);
 
         RateLimiter::for('login', function (Request $request) {
-            $throttleKey = Str::transliterate(Str::lower($request->input(Fortify::username())).'|'.$request->ip());
+            $throttleKey = Str::transliterate(Str::lower($request->input(Fortify::username())) . '|' . $request->ip());
 
             return Limit::perMinute(5)->by($throttleKey);
         });
 
         RateLimiter::for('two-factor', function (Request $request) {
             return Limit::perMinute(5)->by($request->session()->get('login.id'));
+        });
+
+        $this->app->instance(LoginResponse::class, new class implements LoginResponse {
+            public function toResponse($request)
+            {
+                return new JsonResponse(['success' => true, 'message' => 'Logged in'], 200);
+            }
+        });
+
+        // ログアウト成功時のレスポンスをJSONに上書き
+        $this->app->instance(LogoutResponse::class, new class implements LogoutResponse {
+            public function toResponse($request)
+            {
+                return new JsonResponse(['success' => true, 'message' => 'Logged out'], 200);
+            }
+        });
+
+        // 新規登録成功時のレスポンスをJSONに上書き
+        $this->app->instance(RegisterResponse::class, new class implements RegisterResponse {
+            public function toResponse($request)
+            {
+                return new JsonResponse(['success' => true, 'message' => 'Registered'], 201);
+            }
         });
     }
 }
